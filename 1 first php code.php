c7021f4c5346cbd20603a64b2bd737a1de907bd1
karimul-hassan <karimul.hassan09@gmail.com>
2024-10-09 13:09:51 +0600

    first commit

---
+++ 1 first php code.php
@@ -0,0 +1,34 @@
+<?php
+// The 'echo' command prints text on the web page.
+echo "I love Pizza <br>";  // This will display "I love Pizza" followed by a line break (<br>).
+
+echo "It's really good";  // This will display "It's really good" after the previous line.
+
+// This is a single-line comment. Anything written after "//" on this line won't run in the code.
+
+/*
+This is a multiline comment.
+You can write comments over several lines like this,
+and it won't be executed by the code.
+*/
+?>
+
+
+
+<!-- I can write my html inside  php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <button>Order Pizza</button>
+
+</body>
+
+</html>
